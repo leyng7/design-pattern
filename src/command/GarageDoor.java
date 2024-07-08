cd4147737c9ef4b61db4ada187231bce03551f7c
leyng7 <leyng7@gmail.com>
2024-07-08 17:15:04 +0900

    ~ 239p ADD GarageDoorOpenCommand

---
+++ src/command/GarageDoor.java
@@ -0,0 +1,13 @@
+package command;
+
+public class GarageDoor {
+
+    public void up() {
+        System.out.println("GarageDoor is open");
+    }
+
+    public void down() {
+        System.out.println("GarageDoor is close");
+    }
+
+}
--- src/command/RemoteControlTest.java
+++ src/command/RemoteControlTest.java
@@ -4,10 +4,17 @@ public class RemoteControlTest {
 
     public static void main(String[] args) {
         SimpleRemoteControl control = new SimpleRemoteControl();
+
         Light light = new Light();
         LightOnCommand command = new LightOnCommand(light);
 
+        GarageDoor garageDoor = new GarageDoor();
+        GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
+
         control.setCommand(command);
+        control.buttonWasPressed();
+
+        control.setCommand(garageOpen);
         control.buttonWasPressed();
     }
 
